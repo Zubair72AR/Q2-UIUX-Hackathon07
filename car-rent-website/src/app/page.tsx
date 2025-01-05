214f64929174baab58872cca937fe63e97c2ae52
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2025-01-05 17:26:43 +0500

    footer design

---
+++ car-rent-website/src/app/page.tsx
@@ -0,0 +1,10 @@
+import TestButton from "@/components/testButton";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <TestButton />
+    </div>
+  );
+}
---
+++ car-rent-website/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bg-yellow-300 pt-20 pb-12 px-6 md:px-12 lg:px-16 2xl:px-24">
+      <div className="bg-red-500 grid grid-cols-6 gap-12">
+        <div className="bg-orange-200 col-span-3"><h2>MORENT</h2></div>
+        <div className="bg-orange-300">bbbb</div>
+        <div className="bg-orange-400">cccc</div>
+        <div className="bg-orange-500">ddfsfsd fkjshfjks dd</div>
+      </div>
+      <hr />
+      <div className="bg-green-500">b</div>
+    </div>
+  );
+}
